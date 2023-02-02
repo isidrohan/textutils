b55fdf80feaa2382dc21239e2e26eec17da57d4b
Siddharth Rohan <sidrohanme2@gmail.com>
2023-02-02 19:09:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import TextBox from './components/TextBox';
+import Alert from './components/Alert';
+import { useState } from 'react';
+import About from './components/About';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const[BarMode , setBarMode] = useState("light");
+  const[text , setText] = useState("Enable Dark Mode ");
+  const [alert, setalert] = useState(null);
+  const showAlert=(message,type)=>{
+    setalert ({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setalert(null);
+    }, 2000);
+
+  }
+  const togglemode = ()=>{
+    if(BarMode==="light"){
+      setBarMode('dark');
+      document.body.style.background = '#343a40';
+      setText("Disable dark Mode ");
+      showAlert(" Dark Mode has been Enabled ","success");
+    }
+    else{
+      setBarMode('light');
+      document.body.style.background = 'white';
+      setText("Enable Dark Mode");
+      showAlert(" Dark Mode has been Disabled","success");
+    }
+  }
+  return (
+    <>
+      <Router>
+      <NavBar home = "Home" title = "TextUtills" aboutText = "About" mode = {BarMode} togglemode = {togglemode} textMode = {text}></NavBar>
+      <Alert alert = {alert}></Alert>
+      <div className="container my-3">
+      <Routes>
+            <Route path="/about" element = {<About></About>} >
+            </Route>
+            <Route path="/" element = {<TextBox showAlert = {showAlert} heading="Write your notes here" mode = {BarMode}></TextBox> }>  
+            </Route>
+      </Routes>
+      </div>
+      </Router>
+    </>
+  );
+}
+export default App;
---
+++ src/components/TextBox.js
@@ -0,0 +1,96 @@
+import React , {useState} from 'react'
+
+export default function TextBox(props) {
+    const handleLowClick = ()=>{
+      console.log("lower case was clicked" + text)
+      let newText = text.toLowerCase();
+      setText(newText);
+      if (newText==='') {
+        props.showAlert("Enter Any Word To Convert","warning");
+      }
+      else{
+        props.showAlert("Converted To Lowercase","success");
+      }
+
+    }
+    const handleCopyClick = ()=>{
+      console.log("text copied");
+      let newtext = document.getElementById("mybox");
+      newtext.select();
+      navigator.clipboard.writeText(newtext.value);
+      if (newtext===" ") {
+        props.showAlert("No Word To Copy","warning");
+      }
+      else{
+        props.showAlert("Words Has Been Copied","success");
+      }
+    }
+    const handleUpClick = ()=>{
+        console.log("uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        if (newText==='') {
+          props.showAlert("No Word To Convert","warning");
+        }
+        else{
+          props.showAlert("Converted To Upppercase","success");
+        }
+    }
+    const handleClearClick = ()=>{
+        console.log("uppercase was clicked" + text);
+        let newText = " ";
+        setText(newText);
+        if (newText==='') {
+          props.showAlert("No Word To Clear","warning");
+        }
+        else{
+          props.showAlert("Display has been cleared","success");
+        }
+    }
+    const handleCapitalizedClick =()=>{
+      const arr = text.split(" ");
+      for (let i = 0; i < arr.length; i++) {
+         arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);    
+      }
+      let newText = arr.join(" ");
+      setText(newText);
+      if (newText==='') {
+        props.showAlert("No Word To capitalized","warning");
+      }
+      else{
+        props.showAlert("Words has been capitalized","success");
+      }
+    }
+
+    const handleOnChange = (event)=>{
+        console.log("on change");
+        setText(event.target.value);
+    }
+    const[text,setText] = useState("");
+    // setText('new text');
+  return (
+    <>
+    <div className='container' style={{ color: props.mode==='dark'?'white':'black'}}>
+        <h1 >{props.heading} </h1>
+        <div className="mb-3">
+        <textarea className="form-control my-3" style={{ background: props.mode==='dark'?'#343a40':'white',color:props.mode==='dark'?'white':'black'}}  value={text} placeholder="Enter text here..." onChange = {handleOnChange} id="mybox" rows="8"></textarea>
+        <button className="btn btn-success my-3"  onClick={handleUpClick}>Convert To Upper Case</button>
+        <button className="btn btn-success mx-3"  onClick={handleLowClick}>Convert To Lower Case</button>
+        <button className="btn btn-success mx-3"  onClick={handleCapitalizedClick}>Convert To Capitalized Case</button>
+        <button className="btn btn-success mx-3"  onClick={handleClearClick}>Clear Page</button>
+        <button className="btn btn-success mx-3"  onClick={handleCopyClick}>Copy Text</button>
+        </div>
+    </div>
+    <div className="container" style={{ color: props.mode==='dark'?'white':'black'}} >
+      <h2>Your Text Summary</h2>
+      <p>Number of words <b>{text.split(" ").length}</b>
+      <br /> Number of characters <b>{text.length}</b>  </p>
+      <p><b>{0.008 * text.split(" ").length}</b> Minute read</p>
+
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
+
+  )
+}
